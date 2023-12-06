974956de92c235619bef92cd6978faa36edf1f7a
Peter <s@pramos.me>
2023-12-06 18:05:20 +0100

    feat: fast way to solve part 2

--- src/day06.rs
+++ src/day06.rs
@@ -6,20 +6,32 @@ pub fn solve(input: Vec<String>) {
 }
 
 fn part_one(td: Vec<RaceData>) -> usize {
-    td.iter()
-        .map(|r| {
-            (1..r.time)
-                .into_iter()
-                .filter(|n| {
-                    let boat = Boat::new(*n);
-                    boat.beats(r)
-                })
-                .count()
-        })
-        .product()
+    td.into_iter().map(ways_to_win_naive).product()
 }
 
 fn part_two(race: RaceData) -> usize {
+    ways_to_win_quadratic(&race)
+}
+
+// the proper way to solve this is with a quadratic equation.
+// if distance = ( total_time - time_holding ) * time_holding
+// then distance = (total_time * time_holding) - time_holding²
+// this turns out to be a quadratic equation(ax² + bx + c = 0):
+// -time_holding² + total_time * time_holding - distance = 0
+// Because we have total_time (race.time) and distance (race.distance)
+// we can substitute and calculate the values for x, x1 and x2, the min
+// and max amount of time we can hold the button to match the boat.
+//floor
+fn ways_to_win_quadratic(race: &RaceData) -> usize {
+    let t = race.time as f64;
+    let d = race.distance as f64 + 0.01; // <- to beat the record.
+    let root = (t.powi(2) - 4. * d).sqrt();
+    let a = (((-t - root) / -2.).floor()) as usize;
+    let b = (((-t + root) / -2.).ceil()) as usize;
+    a - b + 1
+}
+
+fn ways_to_win_naive(race: RaceData) -> usize {
     (1..race.time)
         .into_iter()
         .filter(|n| {
